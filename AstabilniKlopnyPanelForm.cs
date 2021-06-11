1564ff52702ff696f89f03f44554ff9ea3a8a087
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 14:33:48 +0200

    Initial commit

---
+++ AstabilniKlopnyPanelForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS064AstabilniKlopnyPanel
+{
+    public partial class AstabilniKlopnyPanelForm : Form
+    {
+        public AstabilniKlopnyPanelForm()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void panelAstabilni_Click(object sender, EventArgs e)
+        {
+            (sender as Panel).BackColor = Color.Red;
+
+            Timer t = new Timer();
+            t.Interval = 1000;
+            t.Tick += t_Tick;
+            t.Tag = sender;
+            t.Start();
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            Timer t = (sender as Timer);
+            (t.Tag as Panel).BackColor = Color.White;
+            t.Stop();
+        }
+    }
+}
---
+++ CS064AstabilniKlopnyPanel/AstabilniKlopnyPanelForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace CS064AstabilniKlopnyPanel
+{
+    partial class AstabilniKlopnyPanelForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(93, 132);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(160, 69);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(275, 133);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(160, 71);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(469, 133);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(170, 71);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(370, 241);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(172, 77);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(134, 241);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(160, 77);
+            this.panel5.TabIndex = 1;
+            // 
+            // AstabilniKlopnyPanelForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "AstabilniKlopnyPanelForm";
+            this.Text = "CS064 Astabilní Klopný Panel - Pavel Pekárek";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
